8ecdbaaa28a7dd1809d3c9f8e1053e5c6df7d96b
ohyeah <ttsping@gmail.com>
2021-12-18 18:12:33 +0800

    remove unused code

--- src/flowin_utils.cpp
+++ src/flowin_utils.cpp
@@ -5,19 +5,6 @@
 
 namespace flowin_utils {
 
-bool add_or_remove_window_frame(HWND wnd, bool remove /*= true*/) {
-    LONG style = GetWindowLong(wnd, GWL_STYLE);
-    LONG style_check = WS_CAPTION | WS_THICKFRAME;
-    style = remove ? style & ~style_check : style | style_check;
-    SetWindowLong(wnd, GWL_STYLE, style);
-    return !!SetWindowPos(wnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE | SWP_NOZORDER | SWP_FRAMECHANGED);
-}
-
-bool has_window_frame(HWND wnd) {
-    LONG style = GetWindowLong(wnd, GWL_STYLE);
-    return ((style & WS_CAPTION) != 0) || ((style & WS_THICKFRAME) != 0);
-}
-
 bool is_composition_enabled() {
     BOOL composition_enabled = FALSE;
     return SUCCEEDED(::DwmIsCompositionEnabled(&composition_enabled)) && composition_enabled;
--- src/flowin_utils.h
+++ src/flowin_utils.h
@@ -2,10 +2,6 @@
 
 namespace flowin_utils {
 
-bool add_or_remove_window_frame(HWND wnd, bool remove = true);
-
-bool has_window_frame(HWND wnd);
-
 bool is_composition_enabled();
 
 bool is_maximized(HWND wnd);
