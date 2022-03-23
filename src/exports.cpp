04f4162cf6c0c3173893f8e9b62798b0c8c697be
ohyeah <ttsping@gmail.com>
2022-03-23 22:57:53 +0800

    minor changes

--- src/exports.cpp
+++ src/exports.cpp
@@ -8,18 +8,6 @@ STDAPI DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _Outptr_ LPVOID
         return E_POINTER;
     }
 
-    static wchar_t debugFile[MAX_PATH]{};
-    if (debugFile[0] == '\0') {
-        DWORD len = GetModuleFileNameW(g_module, debugFile, MAX_PATH);
-        debugFile[len] = 0;
-        PathRemoveFileSpecW(debugFile);
-        PathAppendW(debugFile, L"FLOWIN_DISABLE_SCRIPT");
-    }
-
-    if (PathFileExistsW(debugFile)) {
-        return E_FAIL;
-    }
-
     HRESULT hr = E_FAIL;
     if (rclsid == __uuidof(FlowinControlImpl)) {
         FlowinControlImplFactory* factory = new com_object_impl_t<FlowinControlImplFactory>();
--- src/initquit.cpp
+++ src/initquit.cpp
@@ -11,9 +11,6 @@ class flowin_initquit : public initquit {
     void on_quit() {
         flowin_core::get()->finalize();
         flowin_core::get().reset();
-        if (g_ctx_cookie != 0) {
-            DeactivateActCtx(0, g_ctx_cookie);
-        }
     }
 };
 
@@ -21,17 +18,27 @@ class flowin_init_stage : public init_stage_callback {
   public:
     void on_init_stage(t_uint32 stage) {
         if (stage == init_stages::before_ui_init) {
-            wchar_t path[MAX_PATH + 4] = {};
-            (VOID) GetModuleFileNameW(core_api::get_my_instance(), path, MAX_PATH);
-            ACTCTXW ctx{};
-            ctx.cbSize = sizeof(ctx);
-            ctx.dwFlags = ACTCTX_FLAG_RESOURCE_NAME_VALID | ACTCTX_FLAG_HMODULE_VALID;
-            ctx.lpSource = path;
-            ctx.lpResourceName = ISOLATIONAWARE_MANIFEST_RESOURCE_ID;
-            ctx.hModule = core_api::get_my_instance();
-            HANDLE h = CreateActCtxW(&ctx);
-            if (h != INVALID_HANDLE_VALUE) {
-                (VOID)ActivateActCtx(h, &g_ctx_cookie);
+            static wchar_t debugFile[MAX_PATH]{};
+            if (debugFile[0] == '\0') {
+                DWORD len = GetModuleFileNameW(core_api::get_my_instance(), debugFile, MAX_PATH);
+                debugFile[len] = 0;
+                PathRemoveFileSpecW(debugFile);
+                PathAppendW(debugFile, L".flowin_enable_script_object");
+            }
+
+            if (PathFileExistsW(debugFile)) {
+                wchar_t path[MAX_PATH + 4] = {};
+                (VOID) GetModuleFileNameW(core_api::get_my_instance(), path, MAX_PATH);
+                ACTCTXW ctx{};
+                ctx.cbSize = sizeof(ctx);
+                ctx.dwFlags = ACTCTX_FLAG_RESOURCE_NAME_VALID | ACTCTX_FLAG_HMODULE_VALID;
+                ctx.lpSource = path;
+                ctx.lpResourceName = ISOLATIONAWARE_MANIFEST_RESOURCE_ID;
+                ctx.hModule = core_api::get_my_instance();
+                HANDLE h = CreateActCtxW(&ctx);
+                if (h != INVALID_HANDLE_VALUE) {
+                    (VOID) ActivateActCtx(h, &g_ctx_cookie);
+                }
             }
         } else if (stage == init_stages::after_ui_init) {
             flowin_core::get()->initalize();
